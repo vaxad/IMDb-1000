30907d13a0d50e11040c75030b602c732bb04775
vaxad <varadprabhu111@gmail.com>
2023-08-23 20:49:30 +0530

    responsive-update

--- app/page.js
+++ app/page.js
@@ -133,7 +133,7 @@ export default function Home() {
   };
 
   return !data ? (<Loading />) : (
-    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 md:p-10 p-3 mb-10">
+    <main className={`flex min-h-screen flex-col items-center lg:justify-between ${data.length===0?' justify-start':'justify-between'} lg:p-24 md:p-10 p-3 mb-10`}>
       <button onClick={() => { scrollToTop() }} className={`${scrollable ? '' : 'hidden'}  fixed bottom-2 right-2 hover:scale-110 transition-all `}><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" fill="#ffffff" viewBox="0 0 50 50">
         <path d="M 9 4 C 6.2504839 4 4 6.2504839 4 9 L 4 41 C 4 43.749516 6.2504839 46 9 46 L 41 46 C 43.749516 46 46 43.749516 46 41 L 46 9 C 46 6.2504839 43.749516 4 41 4 L 9 4 z M 9 6 L 41 6 C 42.668484 6 44 7.3315161 44 9 L 44 41 C 44 42.668484 42.668484 44 41 44 L 9 44 C 7.3315161 44 6 42.668484 6 41 L 6 9 C 6 7.3315161 7.3315161 6 9 6 z M 25 16.585938 L 12.292969 29.292969 A 1.0001 1.0001 0 1 0 13.707031 30.707031 L 25 19.414062 L 36.292969 30.707031 A 1.0001 1.0001 0 1 0 37.707031 29.292969 L 25 16.585938 z"></path>
       </svg></button>
